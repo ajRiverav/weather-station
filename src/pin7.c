49bb2337054571364941f3eb5a3086c24cf1db1d
AJ Rivera <aj.rivera@jhu.edu>
2017-10-15 20:59:26 -0500

    Modify to use with python

--- src/pin7.c
+++ src/pin7.c
@@ -11,12 +11,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
+
 #define MAXTIMINGS	85
 #define DHTPIN		7
 int dht11_dat[5] = { 0, 0, 0, 0, 0 };
 
 void read_dht11_dat()
 {
+
+
+	bool keepTrying = true;
+
+        while (keepTrying)
+	{
 	uint8_t laststate	= HIGH;
 	uint8_t counter		= 0;
 	uint8_t j		= 0, i;
@@ -67,37 +75,41 @@ void read_dht11_dat()
 	 * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
 	 * print it out if data is good
 	 */
-	if ( (j >= 40) &&
-	     (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
-	{
 
-		struct timeval tv;
-		gettimeofday(&tv,NULL);
-		int tSecs = tv.tv_sec;
+		if ( (j >= 40) &&
+	     	(dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+		{
 
-		f = dht11_dat[2] * 9. / 5. + 32;
-		//printf( "Humidity = %d.%d %% Temperature = %d.%d *C (%.1f *F)\n",
-		printf("%d,%d.%d,%.1f\n",
-			tSecs,dht11_dat[0], dht11_dat[1],f );
-	}/*else  {
-		printf( "Data not good, skip\n" );
-	}*/
+			struct timeval tv;
+			gettimeofday(&tv,NULL);
+			int tSecs = tv.tv_sec;
+
+			f = dht11_dat[2] * 9. / 5. + 32;
+			//printf( "Humidity = %d.%d %% Temperature = %d.%d *C (%.1f *F)\n",
+			printf("%d,%d.%d,%.1f\n",
+				tSecs,dht11_dat[0], dht11_dat[1],f );
+			keepTrying = false;
+		}
+		else  {
+			printf( "Data not good, skip\n" );
+		}
+	}
 }
 
 int main( void )
 {
-//	printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
+	printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
 
 	if ( wiringPiSetup() == -1 )
 		exit( 1 );
 
-	while ( 1 )
-	{
+	//while ( 1 )
+	//{
 
 		read_dht11_dat();
-		delay( 1000 ); /* wait 1sec to refresh */
+	//	delay( 1000 ); /* wait 1sec to refresh */
 		
-	}
+	//}
 
 	return(0);
 }
